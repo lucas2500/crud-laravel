908709663c103f50b707128e61f80d79c2fb574f
unknown <lucasrafael654@gmail.com>
2018-03-22 10:48:45 -0300

    <Primeiro commit neste repo>

---
+++ app/Imovel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Imovel extends Model
+{
+
+	protected $fillable = [
+
+		"descricao", "logradouroEndereco", "bairroEndereco",
+		"numeroEndereco", "cepEndereco", "cidadeEndereco", "preco",
+		"qtdQuartos", "tipo", "finalidade"
+	];
+
+	protected $table = "imoveis";
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+// Chamo o Schema
+use Schema;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Esta linha evita erros nas migrations
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
